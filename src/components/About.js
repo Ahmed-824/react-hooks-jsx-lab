55104afb56a698f062ecabb3bdaf9ea4aa9e5b24
Ahmedaliamour <abdalaamour69@gmail.com>
2024-08-15 22:27:51 +0300

    labs

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Ensure this path is correct and matches your exports
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me and my work.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
